988fb4cbd392773a619d978916b831d0d48abc6b
jouyinhuang <scyjh2@nottingham.ac.uk>
2021-11-02 12:10:17 +0000

    hard-codded inner cross-validation

--- cw1_regression/GridSearchCV.m
+++ cw1_regression/GridSearchCV.m
@@ -10,32 +10,54 @@ function [support_vector_num, optimal_hyperparameters, min_rmse] = GridSearchCV(
     % initialize the best RMSE to be infinite
     min_rmse = Inf;
 
-    data_size = size(y_train, 1);
+    % set up the initial ending index for the validation set
+    validation_end = 0;
+
+    % set up the dataset size (how many observations)
+    dataset_size = size(y_train, 1);
     
     % hyperparameter tuning 
     for i = 1:size(hyperparameters, 1)
-        % check for kernel method type, train model and cross-validating
-        if strcmp(kernel_method, 'gaussian_rbf')
-            mdl = fitrsvm(x_train, y_train, 'KernelFunction','rbf', 'KernelScale', hyperparameters(i, 1), 'BoxConstraint', hyperparameters(i, 2) , 'Standardize', true, 'KFold', k_fold, 'Epsilon', hyperparameters(i, 3));
-        end
-        if strcmp(kernel_method, 'polynomial') 
-            mdl = fitrsvm(x_train, y_train, 'KernelFunction','polynomial', 'PolynomialOrder', hyperparameters(i, 1), 'BoxConstraint', hyperparameters(i, 2) , 'Standardize', true, 'KFold', k_fold, 'Epsilon', hyperparameters(i, 3));
-        end
-        
-        % record the number and percentage of the support vectors for each
-        % model
-        for n = 1:k_fold
-            support_vector_num(i, n, 1) = length(mdl.Trained{n, 1}.SupportVectors);
-            support_vector_num(i, n, 2) = support_vector_num(i, n, 1)/data_size;
-        end
-        
-        % calculate the average rmse
-        rmse = sqrt(kfoldLoss(mdl));
 
-        % record the result
-        if(rmse<=min_rmse)
+        % initialize the RMSE matrix for this set of hyperparameters
+        rmses = zeros(1, k_fold);
+
+        % inner cross-validation
+        for j = 1:k_fold
+            validation_start = validation_end + 1;
+            validation_end = round(dataset_size * j/k_fold);
+            
+            % split the dataset into training and validation
+            x_validation_set = x_train(validation_start:validation_end, :);
+            x_train_set = x_train(~ismember(1:dataset_size, (validation_start:validation_end)), :);
+            y_validation_set = y_train(validation_start:validation_end, :);
+            y_train_set = y_train(~ismember(1:dataset_size, (validation_start:validation_end)), :);
+            
+            % check for kernel method type and train the SVM regression model
+            if strcmp(kernel_method, 'gaussian_rbf')
+                mdl = fitrsvm(x_train_set, y_train_set, 'KernelFunction','rbf', 'KernelScale', hyperparameters(i, 1), 'BoxConstraint', hyperparameters(i, 2), 'Epsilon', hyperparameters(i, 3));
+            end
+            if strcmp(kernel_method, 'polynomial') 
+                mdl = fitrsvm(x_train_set, y_train_set, 'KernelFunction','polynomial', 'PolynomialOrder', hyperparameters(i, 1), 'BoxConstraint', hyperparameters(i, 2), 'Epsilon', hyperparameters(i, 3));
+            end
+            
+            % evaluate the model by predicting on the validation set
+            y_predict = mdl.predict(x_validation_set);
+            rmses(1, j) = sqrt(mean(y_predict - table2array(y_validation_set)).^2);
+
+            % record the number and percentage of the support vectors for each
+            % model
+            support_vector_num(i, j, 1) = length(mdl.SupportVectors);
+            support_vector_num(i, j, 2) = support_vector_num(i, j, 1)/dataset_size;
+        end
+
+        % calculate the average RMSE for this set of hyperparameters
+        average_rmse = mean(rmses);
+
+        % record the result if the model is better than previous
+        if(average_rmse<=min_rmse)
             optimal_hyperparameters(:) = hyperparameters(i, :);
-            min_rmse = rmse;
+            min_rmse = average_rmse;
         end
     end
 end
