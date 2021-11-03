910a39985c9cd2473e87e844bb682387922caf76
scylj1 <scylj1@nottingham.ac.uk>
2021-11-04 00:06:24 +0800

    add normalisation, change rmse to acc

--- cw1_classification/GridSearchCV.m
+++ cw1_classification/GridSearchCV.m
@@ -4,7 +4,7 @@
 %         op_stats - the range of hyper parameters 
 %         kernal_method - 'rbf' or 'polynomial'
 %         k_fold -
-function [optimise_hyperparameters, op_stats, opt_rmse] = GridSearchCV(features, labels, param_grid, kernel_method, k_fold)
+function [optimise_hyperparameters, op_stats, opt_acc] = GridSearchCV(features, labels, param_grid, kernel_method, k_fold)
 
     % sigma for RBF and q for the polynomial kernel; box-constraint C
     optimise_hyperparameters = zeros(1,2);
@@ -14,24 +14,24 @@ function [optimise_hyperparameters, op_stats, opt_rmse] = GridSearchCV(features,
         num_combination = size(hp_combination_set, 1);
         % pre-allocate the memory for op_stats
         op_stats = struct("c", zeros(1,num_combination), "sigma", zeros(1,num_combination),...
-            "rmse", zeros(1,num_combination), "sv_stats", zeros(3,k_fold,num_combination));
+            "acc", zeros(1,num_combination), "sv_stats", zeros(3,k_fold,num_combination));
     elseif kernel_method == "polynomial"
         [c, q] = ndgrid(param_grid.c, param_grid.q);
         hp_combination_set = [c(:) q(:)];
         num_combination = size(hp_combination_set, 1);
         % pre-allocate the memory for op_stats
         op_stats = struct("c", zeros(1,num_combination), "q", zeros(1,num_combination),...
-            "rmse", zeros(1,num_combiation), "sv_stats", zeros(2,k_fold,num_combination));
+            "acc", zeros(1,num_combiation), "sv_stats", zeros(2,k_fold,num_combination));
     else
         error("Invalid kernel method");
     end
     
     data_size = size(labels, 1);
-    opt_rmse = Inf;
+    opt_acc = 0;
     dpf = floor(data_size / k_fold);% data per folder
 
     for i = 1 : num_combination
-        rmses = zeros(1,k_fold);
+        accs = zeros(1,k_fold);
         stats = zeros(k_fold,2);
         
         % cross validation
@@ -55,18 +55,18 @@ function [optimise_hyperparameters, op_stats, opt_rmse] = GridSearchCV(features,
             mdl = fitcsvm(features_train, labels_train, 'KernelFunction',kernel_method, 'KernelScale', hp_combination_set(i, 1), 'BoxConstraint', hp_combination_set(i, 2) , 'Standardize', true);
             labels_predict = mdl.predict(features_test);
             
-            rmses(1,k) = sqrt(mean(labels_test - labels_predict).^2);
+            accs(1,k) = length(find((labels_predict - labels_test) == 0))/length(labels_predict);
             
             stats(k,1) = length(mdl.SupportVectors);
             stats(k,2) = stats(k,1) / data_size;
-            stats(k,3) = rmses(1,k);
+            stats(k,3) = accs(1,k);
         end
         
         % update the minimize rmse
-        ave_rmse = mean(rmses);
-        if (ave_rmse < opt_rmse)
+        ave_acc = mean(accs);
+        if (ave_acc > opt_acc)
             optimise_hyperparameters(:) = hp_combination_set(i, :);
-            opt_rmse = ave_rmse;
+            opt_acc = ave_acc;
         end
         
         % write the data into return value
@@ -77,7 +77,7 @@ function [optimise_hyperparameters, op_stats, opt_rmse] = GridSearchCV(features,
             op_stats(i).q = hp_combination_set(i, 2);
         end
         op_stats(i).sv_stats = stats;
-        op_stats(i).rmse = ave_rmse;
+        op_stats(i).acc = ave_acc;
 
     end
 end
